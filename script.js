bc5cdd63cdc0a63391e9736c6d0464cfa2592d35
RGGH <pisscompany@gmail.com>
2023-07-12 21:59:31 +0100

    tidy up

--- script.js
+++ script.js
@@ -10,19 +10,18 @@ const getInvoice = async () => {
     const data = await invoiceRes.json();
 
     // preimage
-    console.log("Preimage : " + data.invoice.pr);
+    //console.log("Preimage : " + data.invoice.pr);
     const preimage = data.invoice.pr;
-    //const preimage = 'lnbc10n1pj27z6ppp53pymny5lxv20t28shldf5zadfzauh8h6e82znc244v29eamv6w5shp50kncf9zk35xg4lxewt4974ry6mudygsztsz8qn3ar8pn3mtpe50scqzzsxqyz5vqsp5lvxsj3sj5m58hk4rxlh9wce2azs8qdcu6rzs7qua0t6sclm4ptdq9qyyssqpch3gv0w44je20c3k7a9lgrrznsekfe5nnn5kr9g96fjxrzc5ym5kfzfm7mc95qxhvfe08g5v9lg0z05h06hzw7akw4essftq766yjsqjg58wa';
-    //const preimage = 'lnbc10c1pj2a5j0pp5kn5ujru36wvv65peg848us3rsy3e7cr4l67d3957phyah4d0nncshp50kncf9zk35xg4lxewt4974ry6mudygsztsz8qn3ar8pn3mtpe50scqzzsxqyz5vqsp5t0lqm4cf363ec0n3c3630utmgtan0cy9sppedlvy490llpdngwqs9qyyssqrga269z8emnea2mrd7e9s3pucfs7zvhjz7w5s4vt5zgvpt7tf0r4j7ftgfle6w9pdfxd5khck0t6kxzyrjrtnneytgtsj6ljnewxjzcpasxa742';
     try {
         const data = await postPreimage(preimage);
-        console.log(data);
+        //console.log(data);
         return preimage;
     } catch (error) {
         console.error(error);
     }
 
 }
+
 
 const requestPayment = async () => {
     try {
@@ -52,7 +51,6 @@ const requestPayment = async () => {
 
 
 
-
 // Save image first requests a payment from the user
 // then makes the query to OpenAI
 const saveImage = async () => {
@@ -64,7 +62,6 @@ const saveImage = async () => {
     sendQuery()
 
 }
-
 
 
 async function sendQuery() {
