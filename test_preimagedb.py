85ce1306ad1aca62bef1912b9d74f8ec9230350b
RGGH <pisscompany@gmail.com>
2023-07-12 16:17:57 +0100

    i

--- test_preimagedb.py
+++ test_preimagedb.py
@@ -2,10 +2,16 @@ import requests
 
 url = "http://127.0.0.1:5000/preimages"
 
-# New preimage data!
-data = {"preimage": "your_new_preimage"}
+# New preimage data
+data = {
+    "preimage": "your_new_preimage"
+}
 
-response = requests.post(url, data=data)
+headers = {
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, data=json.dumps(data), headers=headers)
 
 if response.status_code == 200:
     print("Preimage created successfully")
