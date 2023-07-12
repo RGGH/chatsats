cf5097f35597f40961e410c138880cf7873a0b24
RGGH <pisscompanygmail.com>
2023-07-12 15:12:54 +0000

    Meh

--- test_preimagedb.py
+++ test_preimagedb.py
@@ -3,7 +3,7 @@ import requests
 url = "http://127.0.0.1:5000/preimages"
 
 # Dummy data
-data = {"preimage": "dummy data"}
+data = {"preimage": "dummydata"}
 
 # Send POST request
 response = requests.post(url, json=data)
