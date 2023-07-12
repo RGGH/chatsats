d07f96765b53330ae8dc9f16fc8f869d5e48d6dc
RGGH <pisscompany@gmail.com>
2023-07-12 15:57:57 +0100

    test sql

---
+++ test_preimagedb.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "http://127.0.0.1:5000/preimages"
+
+# Dummy data
+data = {"preimage": "dummy data"}
+
+# Send POST request
+response = requests.post(url, json=data)
+
+# Check response
+if response.status_code == 200:
+    print("Preimage created successfully")
+else:
+    print("Error:", response.text)
