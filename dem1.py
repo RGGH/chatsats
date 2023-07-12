eb42ba9ae7fc554d0a57694cb74d6ea4c14667c1
RGGH <pisscompany@gmail.com>
2023-07-12 18:01:15 +0100

    iii

--- dem1.py
+++ dem1.py
@@ -59,4 +59,4 @@ app.include_router(langchain_router)
 
 # main
 if __name__ == "__main__":
-    uvicorn.run("dem1:app", port=5000, log_level="info")
+    uvicorn.run("dem1:app", port=5000, log_level="debug")
