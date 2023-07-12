c8005059980897346a59c2b940e30bc969952e5d
RGGH <pisscompany@gmail.com>
2023-07-12 16:15:46 +0100

    meh!

--- test_preimagedb.py
+++ test_preimagedb.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://127.0.0.1:5000/preimages"
 
-# New preimage data
+# New preimage data!
 data = {"preimage": "your_new_preimage"}
 
 response = requests.post(url, data=data)
